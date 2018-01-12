1dbc98b302e5996846baef6fdd3a1be58a39efca
Dan Roberts <dan@adadevelopersacademy.org>
2018-01-11 20:36:06 -0800

    Do the whole thing whoops git is hard sometimes

---
+++ index.js
@@ -0,0 +1,48 @@
+const ROWS = 20;
+const COLS = 20;
+
+const COLORS = [
+  "darkgrey",
+  "darkgoldenrod",
+  "darkorange",
+  "darkgreen",
+  "turquoise",
+  "navy",
+  "mediumvioletred",
+  "salmon",
+  "red",
+];
+
+const render = function render(mouseRow, mouseCol) {
+  const renderStartTime = window.performance.now();
+  requestAnimationFrame(() => {
+    const delta = window.performance.now() - renderStartTime;
+    console.log(`render to RAF: ${delta} ms`);
+  });
+
+  // console.log(`Rendering ${mouseRow}-${mouseCol}`);
+  const main = $('#main');
+  main.empty();
+  for (let r = 0; r < ROWS; r++) {
+    const row = $('<div class="row"></div>');
+    for (let c = 0; c < COLS; c++) {
+      let color;
+      if (r == mouseRow && c == mouseCol) {
+        color = 'selected';
+      } else {
+        color = COLORS[Math.floor(Math.random() * COLORS.length)];
+      }
+      const square = $(`<div class="square ${color}"></div>`);
+      square.on('click', (e) => render(r, c));
+      row.append(square);
+    }
+    main.append(row);
+  }
+
+  const delta = window.performance.now() - renderStartTime;
+  console.log(`render top to bottom: ${delta} ms`);
+};
+
+$(document).ready(() => {
+  render();
+});
